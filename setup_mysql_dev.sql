adf58eac16edca2af74e2efcda6bd70ca893557f
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-20 01:17:02 +0200

    prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,10 +3,10 @@
 -- A new user hbnb_dev (in localhost)
 -- The password of hbnb_dev to hbnb_dev_pwd
 
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`
+GRANT ALL PRIVILEGES ON hbnb_dev_db
 TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT PRIVILEGES ON `performance_schema`
+GRANT SELECT ON `performance_schema`
 TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
