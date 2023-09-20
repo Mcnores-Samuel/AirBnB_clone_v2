8c5f42f59a3669069bbc8a157d3deb723b10ec7c
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-20 02:11:21 +0200

    prepares a MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- prepares a MySQL server for the project:
+-- A database name hbnb_test_db
+-- A new user hbnb_test in localhost
+-- The password of hbnb_dev to hbnb_test_pwd
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.*
+TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.*
+TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
