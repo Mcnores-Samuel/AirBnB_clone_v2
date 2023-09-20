06d9f6af78ddb301e0a557654b60b728d76c7038
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-20 02:05:05 +0200

    prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,12 @@
 -- prepares a MySQL server for the project:
--- A database name (hbnb_dev_db)
--- A new user hbnb_dev (in localhost)
+-- A database name hbnb_dev_db
+-- A new user hbnb_dev in localhost
 -- The password of hbnb_dev to hbnb_dev_pwd
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.*
-TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.*
-TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
