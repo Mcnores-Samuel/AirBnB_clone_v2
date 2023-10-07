c2f8f6923155095cb3037bcd3b3736f0fce132e1
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-07 05:40:27 +0200

    generates a .tgz archive from the contents of the web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module contains a fabric based function to generate
+a .tgz archive from the web_static directory.
+"""
+from fabric.api import local
+from datetime import datetime
+from os import path
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static
+
+    Functionality:
+      - Creates a directory versions
+      - Collects a file in we' '
+
+    Returns:
+        return the archive path if the archive has been correctly
+        generated. Otherwise, it should return None
+    """
+    date = str(datetime.utcnow().date()).replace('-', '')
+    time = str(datetime.utcnow().time()).replace(':', '').split('.')[0]
+    if time[0] == '0':
+        time = time[1:]
+    if not path.exists('versions'):
+        local('mkdir versions')
+    else:
+        print("already exists")
+    arch = "versions/web_static{}{}.tgz".format(date, time)
+    local('tar -czf {} web_static'.format(arch))
+
+    if path.exists(arch):
+        return arch
