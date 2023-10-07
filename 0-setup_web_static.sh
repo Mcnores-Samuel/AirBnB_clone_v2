ff037fbe76d4c784bdbd079de45356e84d8d1f84
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-07 02:47:36 +0200

    sets up web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+# sets up web servers for the deployment of web_static.
+
+DATA_DIR="/data/"
+WEBSTATIC="/data/web_static/"
+RELEASES="/data/web_static/releases/"
+SHARED="/data/web_static/shared/"
+TEST="/data/web_static/releases/test/"
+TEMP_HTML="/data/web_static/releases/test/index.html"
+LINK="/data/web_static/current"
+
+apt-get update
+
+# Installs Nginx if it not already installed
+if [ ! -x "/usr/bin/nginx" ]; then
+	apt-get install -y nginx
+fi
+
+# Creates the folder /data/ if it doesn’t already exist
+if [ ! -d "$DATA_DIR" ]; then
+	mkdir -p "$DATA_DIR"
+fi
+
+# Creates the folder /data/web_static/ if it doesn’t already exist
+if [ ! -d "$WEBSTATIC" ]; then
+	mkdir "$WEBSTATIC"
+fi
+
+# Creates the folder /data/web_static/releases/ if it doesn’t already exist
+if [ ! -d "$RELEASES" ]; then
+	mkdir "$RELEASES"
+fi
+
+# Creates the folder /data/web_static/shared/ if it doesn’t already exist
+if [ ! -d "$SHARED" ]; then
+	mkdir "$SHARED"
+fi
+
+# Creates the folder /data/web_static/releases/test/ if it doesn’t already exist
+if [ ! -d "$TEST" ]; then
+	mkdir "$TEST"
+fi
+
+if [ ! -f "$TEMP_HTML" ]; then
+	echo "Holberton School" > "$TEMP_HTML"
+fi
+
+if [ -L "$LINK" ]; then
+	rm "$LINK"
+fi
+
+ln -sf "$TEST" "$LINK"
+chown -R ubuntu:ubuntu "$DATA_DIR"
+
+printf %s "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        add_header X-Served-By $HOSTNAME;
+	root /data/web_static/current;
+	index index.html index.html;
+
+	location /hbnb_static {
+		alias /data/web_static/current;
+		index index.html index.html;
+	}
+
+	location /redirect_me {
+	        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+
+	error_page 404 /404.html;
+	location /404.html {
+	        root /var/www/nginx-html;
+                internal;
+	}
+}
+" > /etc/nginx/sites-available/default
+service nginx restart
--- web_static/README.md
+++ web_static/README.md
@@ -1,1 +1,30 @@
-This directory implements webpage design.
+# Airbnb Static Web
+
+Welcome to the Airbnb Static Web project! This project aims 
+to recreate the look and feel of the Airbnb website in a static web page format.
+The project is designed to demonstrate front-end development skills and showcase
+the ability to create visually appealing and responsive user interfaces.
+This project is an excellent showcase of front-end development skills and can be
+customized or extended further as a personal portfolio piece.
+
+# Table of Contents
+- [Project Overview](#project-overview)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+
+# Project Overview
+The Airbnb Static Web Page project is a single-page application that emulates
+the appearance of the Airbnb website. It showcases listings, search functionality,
+user profiles, and more, all using HTML and CSS. This project is a perfect way to
+practice and demonstrate front-end development skills, including responsive design, styling, and layout.
+
+# Features
+Visually rich UI with responsive design for various screen sizes.
+Interactive components, such as navigation bars, search forms, and user cards.
+Realistic sample data to mimic Airbnb listings and user profiles.
+User-friendly interface that maintains the aesthetics of the Airbnb website.
+Custom styles and fonts to match the Airbnb branding.
+
+# Technologies Used
+- HTML for structuring the web page content.
+- CSS for styling and layout.
