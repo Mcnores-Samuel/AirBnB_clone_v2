2bc8764478dd2ddc1e180f6cfdb3c55cb45c24c9
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-22 19:17:04 +0200

    advances the power of applying path to urls

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index_page():
+    """Displays Hello HBNB text of an index page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Displays HBNB text to the redirected path"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
