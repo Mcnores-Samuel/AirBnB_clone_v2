11aaa07aca941b4cf49013a6861f937477dc1b47
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-22 20:20:30 +0200

    renders a template html

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application
+
+Routes:
+  /: display “Hello HBNB!”
+ /hbnb: display “HBNB”
+ /c/<text>: display “C ”, followed by the value of the text
+    variable (replace underscore _ symbols with a space )
+ /python/<text>: display “Python ”, followed by the value of
+    the text variable (replace underscore _ symbols with a space )
+"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index_page():
+    """Displays Hello HBNB text of an index page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB text to the redirected path"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def process_text(text):
+    """Applies text processing to the text form end user"""
+    if text:
+        text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def process_text2(text="is cool"):
+    """Applies text processing to the text form end user"""
+    if text:
+        text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_processing(n):
+    """Checks if n post data is a number"""
+    return '{} is a number'.format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Renders a template html if n is an integer"""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
--- web_flask/README.md
+++ web_flask/README.md
@@ -2,28 +2,3 @@
 
 ## Description
 This project is about Web Frameworks. We will be using Flask to build a web application.
-
-## Table of contents
-Files | Description
------ | -----------
-0-hello_route.py | Script that starts a Flask web application.
-1-hbnb_route.py | Script that starts a Flask web application.
-2-c_route.py | Script that starts a Flask web application.
-3-python_route.py | Script that starts a Flask web application.
-4-number_route.py | Script that starts a Flask web application.
-5-number_template.py | Script that starts a Flask web application.
-6-number_odd_or_even.py | Script that starts a Flask web application.
-7-states_list.py | Script that starts a Flask web application.
-8-cities_by_states.py | Script that starts a Flask web application.
-9-states.py | Script that starts a Flask web application.
-10-hbnb_filters.py | Script that starts a Flask web application.
-11-hbnb_filters.py | Script that starts a Flask web application.
-12-hbnb.py | Script that starts a Flask web application.
-100-hbnb.py | Script that starts a Flask web application.
-101-hbnb.py | Script that starts a Flask web application.
-102-hbnb.py | Script that starts a Flask web application.
-103-hbnb.py | Script that starts a Flask web application.
-models/state.py | Class State that inherits from BaseModel.
-models/city.py | Class City that inherits from BaseModel.
-models/amenity.py | Class Amenity that inherits from BaseModel.
-models/place.py | Class Place that inherits from BaseModel.
